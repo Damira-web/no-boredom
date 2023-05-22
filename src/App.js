dc486ab8c0bf7e75a89d9f21fd18be4665f46e17
damira-web <damirash.kadyrova@gmail.com>
2023-05-22 23:00:58 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import video from './hot-balloons.mp4';
+
+// ВАРИАНТ I
+
+// function App() {
+
+//   const [advice, setAdvice] = useState('');
+
+//   useEffect(() => {
+//     const getAdvice = async () => {
+//       const response = await fetch("http://www.boredapi.com/api/activity/");
+//       const data = await response.json();
+//       console.log(data.activity);
+//       setAdvice(data.activity)
+//     } 
+//     getAdvice();
+//   }, [])
+
+//   return (
+//     <div>
+
+//       <div className='container'>
+//         <video autoPlay muted loop><source src={video} type="video/mp4"/></video>
+//         <h1>SAY <spam>NO</spam> TO BOREDOM</h1>
+//       </div>
+
+//       <div className='container'>
+//         <h2>{advice}</h2>
+//         <button onClick={getAdvice}><img src="https://img.icons8.com/?size=512&id=47773&format=png" className='icon' alt='icon'/>GET AN IDEA</button>
+//       </div>
+
+//     </div>
+//   );
+// }
+
+
+//ВАРИАНТ II
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  const [advice, setAdvice] = useState('');
+
+  const getAdvice = async() => {
+    const response = await fetch('http://www.boredapi.com/api/activity/');
+    const data = await response.json();
+    console.log(data.activity);
+    setAdvice(data.activity)
+  }
+
+  useEffect(() => {
+    getAdvice();
+  }, [])
+
+  return(
+    <div>
+
+      <div className='container'>
+        <video autoPlay muted loop><source src={video}/></video>
+        <h1>SAY <spam>NO</spam> TO BOREDOM</h1>
+      </div>
+
+      <div className='container'>
+        <button onClick={getAdvice}><img src="https://img.icons8.com/?size=512&id=47773&format=png" className='icon' alt='icon'/>GET AN ADVICE</button>
+      </div>
+
+      <div className='container'>
+        <h2>{advice}</h2>
+      </div>
+
     </div>
   );
+
 }
+
 
 export default App;
